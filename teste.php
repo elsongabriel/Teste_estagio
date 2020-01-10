d145536e9e55180d8b603167ca4d250641747f27
avscgm <avscgm@cin.ufpe.br>
2020-01-09 21:17:40 -0300

    commit inicial do teste

---
+++ teste.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>teste</title>
+  
+  </head>
+  <body>
+  <!-- header -->
+  <div class= 'section-top pt-3'>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <img src="Recursos/logo-frisabor.png" alt="logo" class="img-logo">
+                <div class="contact">
+                    <!--o telefone vai aqui-->
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+  <!-- franquia -->
+  <div class="section-investiment pt-3">
+    <div class="container">
+        <div class="row d-flex">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                <h2>SEJA NOSSO FRANQUEADO</h2>
+                <span class="invert-orange">INVESTIMENTO</span>
+                <div class="box-investment-info">
+                    <!-- informação sobre o investimento aqui -->
+                    <div class="item">
+                        Taxa de franquia: <br>Lojas: <span class="text-orange">R$40.000,00</span><br>Quiosques:R$ <span class="text-orange">R$20.000,00</span>
+                    </div>
+                    <div class="item separador">Investimento total (sem ponto comerial):<br>entre <span class="text-orange">R$120.000,00</span> e <span class="text-orange">R$230.000,00</span>
+                    </div>
+                    <div class="item">
+                        Retorno previsto: <br>de <span class="text-orange">24</span> a <span class="text-orange">36</span> meses
+                    </div>
+                </div>
+                <hr>
+            </div>
+            <div class="col-12 col-sm-12 col-md-12 col-lg-5 ml-auto">
+                <div class="investment-carousel">
+                    <div id="carouselExampleIndicators" class="carouselSlide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="carouselExampleIndicators" data-slide-to="1" ></li>
+                            <li data-target="carouselExampleIndicators" data-slide-to="2" ></li>        
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active"><img src="Recursos/sundaes.png" alt="sundaes" class="d-block"></div>
+                            <div class="carousel-item"><img src="Recursos/sundaes.png" alt="sundaes" class="d-block"></div>
+                            <div class="carousel-item"><img src="Recursos/sundaes.png" alt="sundaes" class="d-block"></div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+
+</html>
